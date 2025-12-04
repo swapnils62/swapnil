5b858f0d6562ff1518adb6719610421fe6801489
swapnil <swapnils6297@gmail.com>
2025-12-04 21:48:46 +0530

    add application level url file file to project level file

--- swapnil/urls.py
+++ swapnil/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from firstapp import views
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
+    path('firstapp/',include('firstapp.urls')),
 ]
