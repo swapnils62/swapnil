3e87abdc86cc9b8728db90e3ac6cbf204a60b969
swapnil <swapnils6297@gmail.com>
2025-12-04 17:24:56 +0530

    new view create

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def hello(request):
     return HttpResponse("<h1>hello world</h1>")
+def swap(request):
+    return HttpResponse("<h1> new page </h1>")
