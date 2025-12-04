73904814440372e2c09ea0e458f4581d14dcef6a
swapnil <swapnils6297@gmail.com>
2025-12-04 21:49:55 +0530

    multipal views created for one app

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+import datetime
+
 def home(request):
-    return render(request,'html/index.html')
+    a=datetime.datetime.now()
+    return render(request,'html/index.html',{"time":a})
+def good_morning(request):
+    return HttpResponse("goood morning")
+def good_evening(request):
+    return HttpResponse("ggod evening")
+def good_night(request):
+    return HttpResponse("good night")
